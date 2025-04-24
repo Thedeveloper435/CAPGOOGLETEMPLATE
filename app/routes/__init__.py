73c65c7d895a16f8020812d892b95a89ce153568
Maxwell Gibson <maxwellg.@Maxwells-MacBook-Pro.local>
2025-04-23 23:27:12 -0700

    Work done today

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -4,3 +4,5 @@ from .blog import *
 from .user import *
 from .clinic import *
 from .review import *
+from flask import Flask
+
--- app/routes/default.py
+++ app/routes/default.py
@@ -1,7 +1,12 @@
 from app import app
-from flask import render_template
+from flask import render_template # type: ignore
 
 # This is for rendering the home page
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html')
